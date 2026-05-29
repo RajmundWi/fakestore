54bc07725c393b21217750ca9596d8fe6023329c
RajmundWi <100318593+RajmundWi@users.noreply.github.com>
2026-05-29 12:44:01 +0200

    New tests for koszyk

--- tests/test_UI/test_koszyk.py
+++ tests/test_UI/test_koszyk.py
@@ -11,3 +11,39 @@ def test_pusty_koszyk(page: Page, koszyk: Koszyk):
 
     page.goto("https://fakestore.testelka.pl/koszyk/")
     expect(koszyk.get_pusty_koszyk()).to_be_visible()
+
+def test_windsurfing_link(page: Page, koszyk: Koszyk):
+
+    page.goto("https://fakestore.testelka.pl/koszyk/")
+    expect(koszyk.get_windsurfing()).to_be_visible()
+
+def test_wspinaczka_link(page: Page, koszyk: Koszyk):
+
+    page.goto("https://fakestore.testelka.pl/koszyk/")
+    expect(koszyk.get_wspinaczka()).to_be_visible()
+
+def test_wroć_do_sklepu_link(page: Page, koszyk: Koszyk):
+
+    page.goto("https://fakestore.testelka.pl/koszyk/")
+    expect(koszyk.get_wroć_do_sklepu()).to_be_visible()
+
+def test_wroć_do_sklepu_click(page: Page, koszyk: Koszyk):
+
+    page.goto("https://fakestore.testelka.pl/koszyk/")
+    koszyk.get_wroć_do_sklepu().click()
+    expect(page).to_have_url("https://fakestore.testelka.pl/shop/")
+
+def test_windsurfing_click(page: Page, koszyk: Koszyk):
+
+    page.goto("https://fakestore.testelka.pl/koszyk/")
+    koszyk.get_windsurfing().click()
+    expect(page).to_have_url("https://fakestore.testelka.pl/product-category/windsurfing/")
+
+def test_wspinaczka_click(page: Page, koszyk: Koszyk):
+
+    page.goto("https://fakestore.testelka.pl/koszyk/")
+    koszyk.get_wspinaczka().click()
+    expect(page).to_have_url("https://fakestore.testelka.pl/product-category/wspinaczka/")
+
+
+
