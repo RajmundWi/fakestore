23c43050af830f7ea87a5d8654d2b1559cf5a9dc
RajmundWi <100318593+RajmundWi@users.noreply.github.com>
2026-08-28 13:19:32 +0200

    remove comment, add failing test

--- tests/test_UI/test_koszyk.py
+++ tests/test_UI/test_koszyk.py
@@ -8,8 +8,8 @@ def koszyk(page: Page):
     return Koszyk(page)
 
 def test_pusty_koszyk(page: Page, koszyk: Koszyk):
-#asdasdasd
-    page.goto("https://fakestore.testelka.pl/koszyk/")
+
+   # page.goto("https://fakestore.testelka.pl/koszyk/")
     expect(koszyk.get_pusty_koszyk()).to_be_visible()
 
 def test_windsurfing_link(page: Page, koszyk: Koszyk):
